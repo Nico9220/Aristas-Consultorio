8b7a760f368cf4428d91c04f9761a38019b5a736
Nicolas Caretta <nicocaretta_20@hotmail.com>
2022-07-22 14:38:53 -0300

    vamos

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,16 @@ export default function Home() {
 
     return (
         <section>
-            <div>
+            <div className='title-home'>
                 <h1>Bienvenido a Aristas, Odontología integrada.</h1>
+            </div>
+            <div className='title-base'>
+                <h3>
+                    Clínica Odontológica en Plottier
+                    <div>
+                        Abierto hoy hasta las 19:00
+                    </div>
+                </h3>
             </div>
         </section>
     )
