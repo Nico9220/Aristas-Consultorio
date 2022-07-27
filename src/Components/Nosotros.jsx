51d710e88dce2f72260fbf68b72e16c0fd8043e8
Nicolas Caretta <nicocaretta_20@hotmail.com>
2022-07-27 17:50:51 -0300

    sigo

--- src/Components/Nosotros.jsx
+++ src/Components/Nosotros.jsx
@@ -3,8 +3,20 @@ import '../Styles/Nosotros.css';
 
 export default function Nosotros() {
     return (
-        <div>
-            <h1>EQUIPO</h1>
-        </div>
+        <section className='nos-container'>
+            <h1>ODONTOLOGÍA ESPECIALIZADA</h1>
+            <div>
+                <p>Somos un grupo de profesionales de la salud bucal que estamos en contínuo perfeccionamiento para garantizarles la salud y estética que se merecen.</p>
+            </div>
+            <div>Estetica facial, BOTOX y AC. HIALURONICO.</div>
+            <div>Prótesis e Implantes.</div>
+            <div>Cirugía Dentomaxilar.</div>
+            <div>Odontología General niños y adultos.</div>
+            <div>Ortodoncia y Ortopedia.</div>
+            <div>Estética y Blanqueamientos.</div>
+            <div>Periodoncia.</div>
+            <div>Endodoncia (Tratamientos de Conducto).</div>
+            <div>Tarjetas de Crédito y Débito. Mercadopago, Transferencias.</div>
+        </section>
     )
 }
